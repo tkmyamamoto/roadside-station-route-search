e956cd534d103c2e24eea8b79a22874416556868
Takumi Yamamoto <yamamoto.takumi@g.sp.m.is.nagoya-u.ac.jp>
2023-02-26 19:16:18 +0900

    rename file

--- tools/view_spots_html.py
+++ tools/view_spots_html.py
@@ -2,12 +2,11 @@
 # coding: utf-8
 
 import os
-from pprint import pprint
 
 import leafmap.foliumap as leafmap
 
 from rsrs.common.code import get_code
-from rsrs.misc.load_data import convert_geojson_properties, load_geojson
+from rsrs.misc.load_data import convert_geojson_properties
 
 
 def main():
@@ -16,22 +15,10 @@ def main():
 
     if not os.path.exists(converted_geojson):
         convert_geojson_properties(raw_geojson, converted_geojson)
-    # json_data = load_geojson(converted_geojson)["features"]
-    # pprint(json_data[0])
 
     m = leafmap.Map(center=(37, 137), zoom=5)
     m.add_geojson(converted_geojson, layer_name="Roadside Stations")
     m.to_html("./mymap.html")
-
-    # PREFECTURES_JP_CODE
-    # PREFECTURES_EN_CODE
-    # REGION_JP_CODE
-    # REGION_JP_CONTENTS
-    # NAGANO_SPLIT_10
-    # NAGANO_SPLIT_3
-    print(get_code(["PREFECTURES_JP_CODE"])[0])
-
-
 
 
 if __name__ == "__main__":
