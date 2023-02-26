7ba168bdceae20ed2b35df2fd246ffbdf9ae3c5d
Takumi Yamamoto <yamamoto.takumi@g.sp.m.is.nagoya-u.ac.jp>
2023-02-26 18:37:21 +0900

    feat: convert properties topic to Japanese

--- tools/view_spots.py
+++ tools/view_spots.py
@@ -2,14 +2,18 @@ from pprint import pprint
 
 import leafmap.foliumap as leafmap
 
-from rsrs.misc.load_data import load_geojson
+from rsrs.misc.load_data import convert_geojson_properties, load_geojson
 
 
 def main():
-    # pprint(load_geojson("data/P35-18_GML/P35-18_Roadside_Station.geojson"))
+    raw_geojson = "data/P35-18_GML/P35-18_Roadside_Station.geojson"
+    converted_geojson = "data/P35-18_GML/P35-18_Roadside_Station_converted.geojson"
+    convert_geojson_properties(raw_geojson, converted_geojson)
+    # json_data = load_geojson(converted_geojson)["features"]
+    # pprint(json_data[0])
+
     m = leafmap.Map(center=(37, 137), zoom=5)
-    geojson = "data/P35-18_GML/P35-18_Roadside_Station.geojson"
-    m.add_geojson(geojson, layer_name="Roadside Stations")
+    m.add_geojson(converted_geojson, layer_name="Roadside Stations")
     m.to_html("./mymap.html")
 
 
