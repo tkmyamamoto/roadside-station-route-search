807362654d86f41198bc58d7e61c55c1923a35fa
Takumi Yamamoto <yamamoto.takumi@g.sp.m.is.nagoya-u.ac.jp>
2023-02-28 01:42:41 +0900

    upgrade check_get_region_func

--- tests/test.py
+++ tests/test.py
@@ -13,24 +13,31 @@ from rsrs.misc.load_data import convert_geojson_properties, load_geojson
 from rsrs.utils.dict import get_key_from_list_in_value
 
 
-def check_region(json_data):
+def check_get_region_func(json_data):
+    """Check if the name of the region can be found from prefecture and municipality."""
+    municipality_notfound_list = []
     for station in json_data:
         properties = station["properties"]
         prefecture = properties["都道府県名"]
         municipality = properties["市町村名"]
-        print(get_region(prefecture, municipality))
+        region = get_region(prefecture, municipality)
+        if region is None:
+            municipality_notfound_list.append(f"{prefecture} {municipality}")
+    if not list(set(municipality_notfound_list)):
+        print("All checked!!")
+    else:
+        pprint(set(municipality_notfound_list))
+        print(len(set(municipality_notfound_list)))
 
 
 def main():
-    # print(get_region("長野県", "上田市"))
     converted_geojson = "data/P35-18_GML/P35-18_Roadside_Station_converted.geojson"
 
     # if not os.path.exists(converted_geojson):
     #     convert_geojson_properties(raw_geojson, converted_geojson)
     json_data = load_geojson(converted_geojson)["features"]
-    check_region(json_data)
-    # pprint(json_data[0])
-
+    check_get_region_func(json_data)
+    
 
 if __name__ == "__main__":
     main()
