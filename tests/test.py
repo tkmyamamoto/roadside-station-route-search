650ef9b4d091f31f7f2aad29a2b18582058ddd06
Takumi Yamamoto <yamamoto.takumi@g.sp.m.is.nagoya-u.ac.jp>
2023-03-01 03:24:41 +0900

    feat: check consistency for station names

--- tests/test.py
+++ tests/test.py
@@ -5,18 +5,20 @@ import os
 from pprint import pprint
 
 import leafmap.foliumap as leafmap
+import pandas as pd
 import streamlit as st
 
 from rsrs.common.code import get_code
-from rsrs.common.properties import get_region
+from rsrs.common.properties import get_property, get_region
 from rsrs.misc.load_data import convert_geojson_properties, load_geojson
 from rsrs.utils.dict import get_key_from_list_in_value
+from rsrs.utils.str import custom_replace
 
 
 def check_get_region_func(json_data):
     """Check if the name of the region can be found from prefecture and municipality."""
     municipality_notfound_list = []
-    for station in json_data:
+    for station in json_data["features"]:
         properties = station["properties"]
         prefecture = properties["都道府県名"]
         municipality = properties["市町村名"]
@@ -30,13 +32,28 @@ def check_get_region_func(json_data):
         print(len(set(municipality_notfound_list)))
 
 
+def check_consistency_for_station_names(station_names_list_json, station_names_list_csv):
+    station_names_list_json = [custom_replace(s) for s in station_names_list_json]
+    station_names_list_csv = [custom_replace(s) for s in station_names_list_csv]
+    diff = set(station_names_list_json) - set(station_names_list_csv)
+    if diff:
+        pprint(diff)
+        print(f"{len(diff)} stations did not match.")
+    else:
+        print("All checked!!")
+
+
 def main():
     converted_geojson = "data/P35-18_GML/P35-18_Roadside_Station_converted.geojson"
 
     # if not os.path.exists(converted_geojson):
     #     convert_geojson_properties(raw_geojson, converted_geojson)
-    json_data = load_geojson(converted_geojson)["features"]
+    json_data = load_geojson(converted_geojson)
+    print("check_get_region_func")
     check_get_region_func(json_data)
+    df = pd.read_csv("data/list.csv", header=0, index_col=None)
+    print("check_consistency_for_station_names")
+    check_consistency_for_station_names(get_property(json_data, "道の駅名"), list(df["駅 名"]))
 
 
 if __name__ == "__main__":
